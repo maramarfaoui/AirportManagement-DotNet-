028d51e1d4822a7758e3ce5f66a64e54815f3d5d
maramarfaoui <arfaoui.maram@esprit.tn>
2023-02-27 14:16:17 +0100

    tp3

---
+++ AM.Infrastructure/AMContext.cs
@@ -0,0 +1,26 @@
+﻿using ConsoleApp4.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Infrastructure
+{
+    internal class AMContext: DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Plane> Planes { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Traveller> Travellers { get; set; }
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+            Initial Catalog=MaramArfaouiDB;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
